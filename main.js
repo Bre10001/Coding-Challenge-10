ef44585adb3b4397f07322aeb4dcfa96467cc8c6
Breanna Burris <breanna1burris@gmail.com>
2024-10-20 01:14:23 -0400

    Create HTML structure for product page

---
+++ main.js
@@ -0,0 +1,10 @@
+
+// Declaration of price and sizing
+const priceElement = document.getElementById("product-price");
+const sizeSelector = document.getElementById("size-selector");
+
+
+sizeSelector.addEventListener("change", (event) => {
+const selectedPrice = event.target.value;
+priceElement.textContent = `$${selectedPrice}`;
+});
